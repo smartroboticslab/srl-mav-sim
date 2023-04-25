0b7a6698ef5fe1b96cd579c39e1aa527b7eaa93f
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2023-04-25 23:25:52 +0300

    Make extra sure ignition gazebo isn't left running

--- srl_sim_gazebo_ignition/scripts/px4_sitl_ignition.bash
+++ srl_sim_gazebo_ignition/scripts/px4_sitl_ignition.bash
@@ -7,12 +7,15 @@ world="$2"
 airframe="$3"
 build_dir="$px4_dir/build/px4_sitl_default"
 
+# Kill any running instances of gazebo
+pkill -x -KILL ign
+
 export IGN_GAZEBO_RESOURCE_PATH=$px4_dir/../srl_sim_gazebo_ignition/resources:$IGN_GAZEBO_RESOURCE_PATH
 
 source "$px4_dir/Tools/setup_ignition.bash" "$px4_dir" "$build_dir"
 ign gazebo --force-version 6 ${HEADLESS+-s} -r "$world" &
 # Kill Ignition Gazebo once the script exits.
 # shellcheck disable=SC2064
-trap "kill $! || true" EXIT
+trap "kill -9 $! || true" EXIT
 
 env PX4_SIM_MODEL="$airframe" "$build_dir/bin/px4" -d -s etc/init.d-posix/rcS "$build_dir/etc"
