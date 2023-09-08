41cef0464b2bc7ee4d39f087959a9c013d281037
SebsBarbas <sebastian.barbas@tum.de>
2023-09-08 11:35:41 +0200

    New autogenerated simulation, added script to have new world and new models to be able to generate the world

--- srl_sim_gazebo_ignition/scripts/autogenerate_sim_sdf.py
+++ srl_sim_gazebo_ignition/scripts/autogenerate_sim_sdf.py
@@ -16,7 +16,6 @@ import cv2 as cv
 import tqdm
 
 
-
 class Tree:
     def __init__(self, xml_text: str, orientation_distributions: list, scale_distr: list):
         self.text = xml_text
@@ -36,7 +35,7 @@ class Tree:
     
     def generate_random_albedo(self, line: str)->str:
         sections = line.split(".")
-        sections[0] = sections[0] + str(random.randint(0, 4))
+        sections[0] = sections[0] + str(random.randint(0, 2))
         return ".".join(sections)
     
     def generate_text(self, position: list)->str:
@@ -65,12 +64,7 @@ class Tree:
                     
         return "\n".join(lines)
     
-class Rock(Tree):
-    def generate_random_albedo(self, line: str)->str:
-        return line
-    
 def include_rmf_owl_model(pose: list)->str:
-    
 
     ret = f'''
     \n
@@ -190,12 +184,16 @@ def obtain_header(path_to_world: str)->str:
                           <size>2</size>
                         </texture>
                         <blend>
-                          <min_height>0.0</min_height>
+                          <min_height>0.5</min_height>
                           <fade_dist>1.6</fade_dist>
                         </blend>
                         <blend>
-                          <min_height>0.0</min_height>
+                          <min_height>1.0</min_height>
                           <fade_dist>3</fade_dist>
+                        </blend>
+                        <blend>
+                          <min_height>2.9</min_height>
+                          <fade_dist>10</fade_dist>
                         </blend>
                         <uri>{path_to_world}/materials/textures/heightmap.png</uri>
                         <size>128 128 10</size>
@@ -291,18 +289,18 @@ def next_position(probability_map, var = 3):
 
     return ptx, pty, probability_map
 
-#Forest density value extracted from https://ourworldindata.org/grapher/number-of-trees-per-km, for swiss forest https://www.lfi.ch/publikationen/publ/posterserie_LFI3_A4-en.pdf
+
 parser = argparse.ArgumentParser(description='Digiforest Simulator command line arguments')
-parser.add_argument('--num_trees', type=int, default=620, help='Number of trees to load into our terrain')
-parser.add_argument('--save_sdf_path', type=str, default=os.path.join(os.environ["HOME"], "Documents/srl_navigation_ws/src/srl-mav-sim/srl_sim_gazebo_ignition/resources/worlds"), 
+parser.add_argument('--num_trees', type=int, default=100, help='Number of trees to load into our terrain')
+parser.add_argument('--save_sdf_path', type=str, default=os.path.join(os.environ["HOME"], "srl-mav-sim-ws/src/srl-mav-sim/srl_sim_gazebo_ignition/resources/worlds"), 
                                                     help="Argument that specifies the path were the final .sdf file of the world will be saved")
 parser.add_argument("--resources_path", type=str, default=os.path.join(os.environ["HOME"],
-                                            "Documents/srl_navigation_ws/src/srl-mav-sim/srl_sim_gazebo_ignition/resources/models"
+                                            "srl-mav-sim-ws/src/srl-mav-sim/srl_sim_gazebo_ignition/resources/models"
                                             ), help="Argument to allow the user specify where the tree models are located in his computer")
 
 parser.add_argument('--terrain_model_path', type=str,
                                     default=os.path.join(os.environ["HOME"],
-                                            "Documents/srl_navigation_ws/src/srl-mav-sim/srl_sim_gazebo_ignition/resources/models/forest"
+                                            "srl-mav-sim-ws/src/srl-mav-sim/srl_sim_gazebo_ignition/resources/models/forest"
                                             ),
                                     help="Where to store the terrain's model heightmap image")
 parser.add_argument("--seed", type=int, default=15,
@@ -314,9 +312,9 @@ map_shape = [128, 128, 10]
 random.seed(args.seed)
 
 #If there is no heightmap we really want to use, we generate one
-#terrain_map_image = generate_heightmap(map_shape[-1])
+terrain_map_image = generate_heightmap(map_shape[-1])
 terrain_map_image = np.zeros([128, 128])
-#terrain_map_image = (terrain_map_image / terrain_map_image.max()) / 4
+#terrain_map_image = (terrain_map_image / terrain_map_image.max()) / 2
 terrain_map_image[terrain_map_image.shape[0] - 9 : 
                   terrain_map_image.shape[0], :8] = terrain_map_image[terrain_map_image.shape[0] - 9: terrain_map_image.shape[0], :8].mean()
 cv.imwrite(os.path.join(args.terrain_model_path, "materials/textures/heightmap.png"), (terrain_map_image * 255).astype('uint8'))
@@ -324,29 +322,22 @@ cv.imwrite(os.path.join(args.terrain_model_path, "materials/textures/heightmap.p
 
 
 final_xml = obtain_header("model://models/forest")
-final_xml += include_rmf_owl_model([-62, -62, terrain_map_image[terrain_map_image.shape[0] - 3, 2] * 10 + 0.11, 0, 0, 0])
+final_xml += include_rmf_owl_model([-62, -62, terrain_map_image[terrain_map_image.shape[0] - 3, 2] * 10 + 0.1, 0, 0, 0])
 print("Terrain max is " + str(terrain_map_image.max()))
 
 oak_tree_base = open(os.path.join(args.resources_path, "oak_tree/model.txt"), "r").read()
 pine_tree_base = open(os.path.join(args.resources_path, "pine_tree/model.txt"), "r").read()
-rock_o_base = open(os.path.join(args.resources_path, "rock_1/model.txt"), "r").read()
 
 trees = {"oak": {"object": Tree(oak_tree_base,
                                 [st.norm(0, np.pi / 18), st.norm(0, np.pi / 18), st.uniform(-np.pi, 2 * np.pi)],
-                                [st.norm(0.7, 0.1), st.norm(0.7, 0.1), st.norm(2, 0.2)]), 
+                                [st.norm(0.7, 0.1), st.norm(0.7, 0.1), st.norm(1, 0.2)]), 
                 "probability": 0.6,
                 "z-offset": 1.0},
         "pine": {"object": Tree(pine_tree_base,
                                 [st.norm(0, np.pi / 18), st.norm(0, np.pi / 18), st.uniform(-np.pi, 2 * np.pi)],
-                                [st.norm(1, 0.1), st.norm(1, 0.1), st.norm(2.5, 0.2)]),
+                                [st.norm(1, 0.1), st.norm(1, 0.1), st.norm(1.5, 0.2)]),
                 "probability": 0.4,
                 "z-offset": 3.0}}
-
-rocks = {'rock0': {"object": Rock(rock_o_base,
-                                [st.norm(-np.pi / 2, np.pi / 4), st.norm(0, np.pi / 4), st.uniform(-np.pi, 2 * np.pi)],
-                                [st.norm(1, 0.1), st.norm(1, 0.1), st.norm(1, 0.2)]), 
-                "probability": 1.0,
-                "z-offset": -1}}
 
 probabilities = np.ones(terrain_map_image.shape)
 probabilities[ :8, :8] = 0 #Takeoff pose, we do not want trees here
@@ -360,7 +351,7 @@ for i in range(args.num_trees):
     for key in trees:
         cdf += trees[key]["probability"]
         if cdf >= prob:
-            x, y, probabilities = next_position(probabilities, 3)
+            x, y, probabilities = next_position(probabilities, 0.5)
             #x, y = xy[i]
             #z = terrain_map_image[y, x]
             z = terrain_map_image[terrain_map_image.shape[0] - 1 - y, x] #Mirrored in y-axis. Ask Gazebo
@@ -373,25 +364,6 @@ for i in range(args.num_trees):
             final_xml += "\n"
             break
     
-
-for i in range(0):
-    prob = random.uniform(0, 1)
-    cdf = 0.0
-    for key in rocks:
-        cdf += rocks[key]["probability"]
-        if cdf >= prob:
-            x, y, probabilities = next_position(probabilities, 9)
-            #x, y = xy[i]
-            #z = terrain_map_image[y, x]
-            z = terrain_map_image[terrain_map_image.shape[0] - 1 - y, x] #Mirrored in y-axis. Ask Gazebo
-            x_world = (x + 0.5)/ terrain_map_image.shape[1] * 128.0 - 128.0 / 2
-            y_world = (y + 0.5)/ terrain_map_image.shape[0] * 128.0 - 128.0 / 2
-            z = z * 10 + 1.0
-            final_xml += rocks[key]["object"].generate_text([x_world,
-                                                             y_world, 
-                                                             z])
-            final_xml += "\n"
-            break
 
 
 
