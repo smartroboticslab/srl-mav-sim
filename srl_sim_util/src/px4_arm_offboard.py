a61114a3f782cf8d98cd71435359b8298b6038fe
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2024-02-16 18:45:11 +0100

    srl_sim_util: add node to arm and switch to offboard
    Useful for taking-off automatically in simulation.

---
+++ srl_sim_util/src/px4_arm_offboard.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2024 Smart Robotics Lab, Imperial College London, Technical University of Munich
+# SPDX-FileCopyrightText: 2024 Sotiris Papatheodorou
+# SPDX-License-Identifier: BSD-3-Clause
+import mavros_msgs.srv
+import rospy
+
+
+def loginfo(s):
+    rospy.loginfo(rospy.get_name() + ": " + s)
+
+
+if __name__ == '__main__':
+    rospy.init_node('px4_arm_offboard')
+
+    loginfo('Waiting for MAVROS services')
+    rospy.wait_for_service('/mavros/cmd/arming')
+    arm_srv = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)
+    rospy.wait_for_service('/mavros/set_mode')
+    mode_srv = rospy.ServiceProxy('/mavros/set_mode', mavros_msgs.srv.SetMode)
+
+    rate = rospy.Rate(10);
+
+    while not arm_srv(True).success:
+        rate.sleep()
+    loginfo('MAV armed')
+
+    while not mode_srv(0, 'OFFBOARD').mode_sent:
+        rate.sleep()
+    loginfo('MAV switched to OFFBOARD mode')
